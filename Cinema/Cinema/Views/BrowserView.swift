97734d1996696515c772c27666cd6b4246661fc5
mohamdan <engineerm86@gmail.com>
2023-06-10 20:52:49 +0300

    Design Browser View file

--- Cinema/Cinema/Views/BrowserView.swift
+++ Cinema/Cinema/Views/BrowserView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct BrowserView: View {
+    //: Properties
+    var movies : [MovieModel] = decode(file: "movies.json")
+    //: Body
     var body: some View {
-        Text("Browser View")
+        NavigationView {
+            VStack{
+                List {
+                    CoverView()
+                        .frame(height: 235)
+                    ForEach(movies) { item in
+                        MovieCellView(movie: item)
+                    }
+                    HStack {
+                        Spacer()
+                        Image("camera")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 150)
+                        Spacer()
+                    }//: Hstack image
+                    HStack {
+                        Spacer()
+                        Text("All Rights Reserved")
+                            .font(.footnote)
+                        Spacer()
+                    }//: Hstack text
+                }//: List main
+            }//: Vstack
+            .navigationTitle("Cinema")
+        }//: Navigation View
     }
 }
 
 struct BrowserView_Previews: PreviewProvider {
     static var previews: some View {
         BrowserView()
+            .preferredColorScheme(.dark)
     }
 }
