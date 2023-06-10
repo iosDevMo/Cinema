ef1e9e937e94e0b3a27079c1b39a1985b441f858
mohamdan <engineerm86@gmail.com>
2023-06-10 16:02:24 +0300

    add image assets

---
+++ Cinema/Cinema/CinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaApp.swift
+//  Cinema
+//
+//  Created by mohamdan on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
